470768d991a171c4acdc80b72cf2dd1b5c7b1a19
Angie <110476721+AngieGen@users.noreply.github.com>
2023-11-21 23:40:18 +0200

    final design and unnecessary things has been removed

--- src/app.js
+++ src/app.js
@@ -1,25 +1,5 @@
-// Converting °F->°C
-let fahrenheit = document.querySelector("#fahrenheit-link");
-function fahrenheitToCelsius(event) {
-  event.preventDefault();
-  let temperature = document.querySelector("#current-degrees");
-  let celsisusTemp = temperature.innerHTML;
-  temperature.innerHTML = Math.round((celsisusTemp * 9) / 5 + 32);
-}
-fahrenheit.addEventListener("click", fahrenheitToCelsius);
-// Converting celsius to fahrengeit
-let celsius = document.querySelector("#celcius-link");
-function celsiusToFahrenheit(event) {
-  event.preventDefault();
-  let temperature = document.querySelector("#current-degrees");
-  let celsisusTemp = temperature.innerHTML;
-  temperature.innerHTML = Math.round(((celsisusTemp - 32) * 5) / 9);
-}
-celsius.addEventListener("click", celsiusToFahrenheit);
-
-// Current date
+// FORMATING CURRENT DAY, HOURS AND MINUTES
 let now = new Date();
-let currentDate = document.querySelector("#current-date");
 let hours = now.getHours();
 let minutes = now.getMinutes();
 let days = [
@@ -37,85 +17,63 @@ if (minutes < 10) {
   minutes = `0${minutes}`;
 }
 
-currentDate.innerHTML = `${day} ${hours}:${minutes}`;
+document.querySelector(
+  "#current-date"
+).innerHTML = `${day} ${hours}:${minutes}`;
 
+// RESPONSE TO SHOW CURRENT DATA
 function showTemperature(response) {
-  console.log(response.data);
-  document.querySelector("#current-city").innerHTML = response.data.name;
-  let temperature = Math.round(response.data.main.temp);
-  let temperatureElement = document.querySelector("#current-degrees");
-  temperatureElement.innerHTML = `${temperature}`;
+  document.querySelector("#current-city").innerHTML = response.data.city;
 
+  document.querySelector("#current-degrees").innerHTML = Math.round(
+    response.data.temperature.current
+  );
   document.querySelector("#current-weather").innerHTML =
-    response.data.weather[0].description;
+    response.data.condition.description;
 
-  document.querySelector("#maxTemp").innerHTML = Math.round(
-    response.data.main.temp_max
-  );
-  document.querySelector("#minTemp").innerHTML = Math.round(
-    response.data.main.temp_min
-  );
   document.querySelector("#current-humidity").innerHTML =
-    response.data.main.humidity;
-  document.querySelector("#current-wind-speed").innerHTML = Math.round(
-    response.data.wind.speed
-  );
+    response.data.temperature.humidity;
+  document.querySelector("#current-wind-speed").innerHTML =
+    response.data.wind.speed;
   document.querySelector(
     "#icon"
-  ).innerHTML = `<img src="https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png" alt="Sunny" />`;
-}
-function searchCity(city) {
-  let apiKey = "320a6e66b85389b30ab1efe865b1d5b7";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(`${apiUrl}`).then(showTemperature);
+  ).innerHTML = `<img src="${response.data.condition.icon_url}" />`;
+  console.log(showTemperature);
+
+  getForecast(response.data.city);
 }
 
-// Searching for city
-let form = document.querySelector("#search-form");
+// SEARCH CITY USING API
+function searchCity(city) {
+  let apiKey = "06t6cbfb24d9a82130oc6e45cee5e049";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+// INPUT FORM FOR CITY
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-input");
-  let city = document.querySelector("#current-city");
-  city.innerHTML = searchInput.value;
+  document.querySelector("#current-city").innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
-form.addEventListener("submit", search);
-
-// Current location
-function showPosition(position) {
-  let apiKey = "320a6e66b85389b30ab1efe865b1d5b7";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(`${apiUrl}`).then(showTemperature);
-  //console.log(position.coords.latitude);
-  //console.log(position.coords.longitude);
-}
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(showPosition);
-}
-let getCurrentLocationButton = document.querySelector(".current-button");
-getCurrentLocationButton.addEventListener("click", getCurrentLocation);
-// Default city
-searchCity("Rīga");
+document.querySelector("#search-form").addEventListener("submit", search);
 
 // FORECAST FOR THE WEEK (NEXT DAYS)
-
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
   return days[date.getDay()];
 }
 
 function getForecast(city) {
-  let apiKeySheCodes = "06t6cbfb24d9a82130oc6e45cee5e049";
-  let apiUrlSheCodes = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKeySheCodes}&units=metric`;
-  axios(`${apiUrlSheCodes}`).then(displayForecast);
+  let apiKey = "06t6cbfb24d9a82130oc6e45cee5e049";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
@@ -143,4 +101,5 @@ function displayForecast(response) {
   let forecast = document.querySelector("#forecast-for-next-days");
   forecast.innerHTML = forecastHtml;
 }
-getForecast("riga");
+// Default city
+searchCity("Rīga");
